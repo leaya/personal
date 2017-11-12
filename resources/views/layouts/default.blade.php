d5116246f59a3447332930d924012156e430e2d5
lihaoxin <331926815@qq.com>
2017-11-12 00:56:10 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,21 +9,12 @@
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-<header class="navbar navbar-fixed-top navbar-inverse">
+@include('layouts._header')
     <div class="container">
-        <div class="col-md-10 col-md-offset-1">
-            <a href="/" id="logo">Sample App</a>
-            <nav>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="/help">帮助</a></li>
-                    <li><a href="#">登录</a></li>
-                </ul>
-            </nav>
-        </div>
+        <div class="col-md-offset-1 col-md-10">
+            @yield('content')
+            @include('layouts._footer')
     </div>
-</header>
-<div class="container">
-    @yield('content')
 </div>
 </body>
 </html>
