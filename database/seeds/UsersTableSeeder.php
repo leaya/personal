b65cf56f99a3d8826f49dca74b9addb2eb6712c5
lihaoxin <331926815@qq.com>
2017-11-13 05:15:41 +0000

    Add account activations

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,5 +14,13 @@ class UsersTableSeeder extends Seeder
     {
         $users =factory(User::class)->times(50)->make();
         User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
+
+        $user = User::find(1);
+        $user->name = '顺鑫如意';
+        $user->email = '331926815@qq.com';
+        $user->password = bcrypt('password');
+        $user->is_admin = true;
+        $user->activated = true;
+        $user->save();
     }
 }
